751ea69f559af9bbbd3ae11d34bce90b4e6449e6
Andrew Souza <drewmargielaa@gmail.com>
2020-08-13 17:25:02 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import './App.css'
+import TinderCard from './react-tinder-card/index'
+
+const db = [
+  {
+    name: 'Richard Hendricks',
+    url: './img/richard.jpg'
+  },
+  {
+    name: 'Erlich Bachman',
+    url: './img/erlich.jpg'
+  },
+  {
+    name: 'Monica Hall',
+    url: './img/monica.jpg'
+  },
+  {
+    name: 'Jared Dunn',
+    url: './img/jared.jpg'
+  },
+  {
+    name: 'Dinesh Chugtai',
+    url: './img/dinesh.jpg'
+  },
+  {
+    name: 'Andrew Souza',
+    url: './img/andrew.jpg'
+  },
+  {
+    name: 'Megan',
+    url: './img/megan.jpg'
+  },
+  {
+    name: 'Carlos',
+    url: './img/carlos.jpg'
+  },
+  {
+    name: 'Mario',
+    url: './img/mario.jpg'
+  },
+]
+
+function App () {
+  const characters = db
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
+
+  return (
+    <div>
+      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+      <h1>Find Your Study Buddy</h1>
+      <div className='cardContainer'>
+        {characters.map((character) =>
+          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+              <h3>{character.name}</h3>
+            </div>
+          </TinderCard>
+        )}
+      </div>
+      {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+    </div>
+  )
+}
+
+export default App
